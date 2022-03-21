a1f58acc745343dad0b7eb51673d9829c1ff6eda
James Tan <txrtanzi@gmail.com>
2022-03-21 10:21:09 +0000

    init

---
+++ src/test/java/com/tanzi/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanzi.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
